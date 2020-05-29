6c316d5405fa9c4352602ca8a4caf4ac361955b4
T3aHat <secwhiteahat@gmail.com>
2020-05-29 22:41:48 +0900

    add fullscreen mode

--- JikkyoAlways.py
+++ JikkyoAlways.py
@@ -147,20 +147,17 @@ def get_newcomment():
         if (len(results) > 0):  # resultsがあればlatestid更新.ない場合は前のまま
             latestid = results[0].id
         if(realtime):
+            results = api.search(q=word, count=lefted.count(True))
+        else:
             results = api.search(
                 q=word, count=lefted.count(True), since_id=latestid)
-        else:
-            results = api.search(q=word, count=lefted.count(True))
         labels[0].after(5050, get_newcomment)
     else:  # 流れ切ったのがないからnmsごとにleftedがあるか見直す
         labels[0].after(100, get_newcomment)
 
 
 def update_comment():  # resultsを元にコメを書き換え
-    global j
-    global results
-    global nowdatalist
-    global colour
+    global j, results, nowdatalist, colour, rmwd
     k = 0
     for i in range(num_comment):
         if(k != 0 and k % 2 == 0):
@@ -169,7 +166,13 @@ def update_comment():  # resultsを元にコメを書き換え
         if(lefted[i] == True):  # went left.So update comment
             while(j < len(results)):
                 if (exURL(results[j]) and (not longtxt(results[j]))):
-                    nowdatalist[i].text = results[j].text.replace("\n", " ")
+                    if (rmwd):
+                        nowdatalist[i].text = results[j].text.replace(
+                            "\n", "").replace(word.replace(" -filter:retweets", ""), "")
+                    else:
+                        nowdatalist[i].text = results[j].text.replace(
+                            "\n", "")
+
                     nowdatalist[i].id = results[j].id
                     labels[i].place_forget()
                     try:  # colourに例外が来た時のため
@@ -199,10 +202,10 @@ def Change_word(f):
         subf.geometry("300x500")
         subf.title("Settings")
 
-        label1 = tkinter.Label(subf, text="Search Word")
+        label1 = tkinter.Label(subf, text="Search word")
         label1.pack()
         txt1 = tkinter.Entry(subf, width=30)
-        txt1.insert(tkinter.END, word.replace("-filter:retweets", ""))
+        txt1.insert(tkinter.END, word.replace(" -filter:retweets", ""))
         txt1.pack()
 
         bl1 = tkinter.BooleanVar(subf)
@@ -230,6 +233,12 @@ def Change_word(f):
         CheckBox5 = tkinter.Checkbutton(
             subf, text="realtime mode", variable=bl5)
         CheckBox5.pack()
+
+        bl6 = tkinter.BooleanVar(subf)
+        bl6.set(rmwd)
+        CheckBox6 = tkinter.Checkbutton(
+            subf, text="remove Search word", variable=bl6)
+        CheckBox6.pack()
 
         label2 = tkinter.Label(subf, text="num of comments(reccomend:10~25)")
         label2.pack()
@@ -272,12 +281,12 @@ def Change_word(f):
         button = tkinter.Button(subf, text="Apply")
         button.pack(side="right")
         button.bind("<1>", lambda word: get_word(f, txt1, bl1, bl2,
-                                                 bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, txt7))
+                                                 bl3, bl4, bl5, bl6, txt2, txt3, txt4, txt5, txt6, txt7))
 
         button = tkinter.Button(subf, text="Default")
         button.pack(side="left")
         button.bind("<1>", lambda word: get_default(f, txt1, bl1, bl2,
-                                                    bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, txt7))
+                                                    bl3, bl4, bl5, bl6, txt2, txt3, txt4, txt5, txt6, txt7))
 
         subf.mainloop()
     return x
@@ -296,7 +305,7 @@ def tweetframe(word):
         twbl1.set(True)
         twf.bind("<Control-Return>", lambda word: tweet(twtxt1, twbl1))
         twCheckBox1 = tkinter.Checkbutton(
-            twf, text="append "+word.replace("-filter:retweets", ""), variable=twbl1)
+            twf, text="append "+word.replace(" -filter:retweets", ""), variable=twbl1)
         twCheckBox1.pack(side='left')
 
         twbtn = tkinter.Button(twf, text="Tweet", bg='#1DA1F2')
@@ -310,7 +319,7 @@ def tweetframe(word):
 def tweet(twtxt1, twbl1):
     text = twtxt1.get('1.0', 'end -1c')
     if(twbl1.get()):
-        text += "\n"+word.replace("-filter:retweets", "")
+        text += "\n"+word.replace(" -filter:retweets", "")
     try:
         api.update_status(text)
         twtxt1.delete('1.0', 'end')
@@ -321,8 +330,7 @@ def tweet(twtxt1, twbl1):
         print(e)
 
 
-def get_default(f, txt1, bl1, bl2, bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, txt7):
-
+def get_default(f, txt1, bl1, bl2, bl3, bl4, bl5, bl6, txt2, txt3, txt4, txt5, txt6, txt7):
     txt1.delete(0, tkinter.END)
     txt1.insert(tkinter.END, "Change here")
     bl1.set(True)
@@ -330,6 +338,7 @@ def get_default(f, txt1, bl1, bl2, bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, 
     bl3.set(False)
     bl4.set(False)
     bl5.set(False)
+    bl6.set(True)
     txt2.delete(0, tkinter.END)
     txt2.insert(tkinter.END, "25")
     txt3.delete(0, tkinter.END)
@@ -344,8 +353,8 @@ def get_default(f, txt1, bl1, bl2, bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, 
     txt7.insert(tkinter.END, "white")
 
 
-def get_word(f, txt1, bl1, bl2, bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, txt7):
-    global word, withoutURL, withoutRT, enable_fav, enable_rt, realtime, num_comment, fontsize, max_length, v, acc, colour
+def get_word(f, txt1, bl1, bl2, bl3, bl4, bl5, bl6, txt2, txt3, txt4, txt5, txt6, txt7):
+    global word, withoutURL, withoutRT, enable_fav, enable_rt, realtime, rmwd, num_comment, fontsize, max_length, v, acc, colour
 
     withoutURL = bl1.get()
     withoutRT = bl2.get()
@@ -355,6 +364,7 @@ def get_word(f, txt1, bl1, bl2, bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, txt
     enable_fav = bl3.get()
     enable_rt = bl4.get()
     realtime = bl5.get()
+    rmwd = bl6.get()
     num_comment = int(txt2.get())
     if(txt3.get() == "rec"):
         i = 1
@@ -386,12 +396,25 @@ def get_word(f, txt1, bl1, bl2, bl3, bl4, bl5, txt2, txt3, txt4, txt5, txt6, txt
     print("enable_fav:"+str(enable_fav))
     print("enable_rt:"+str(enable_rt))
     print("realtime mode:"+str(realtime))
+    print("remove Search word:"+str(rmwd))
     print("num_comment:"+str(num_comment))
     print("fontsize:"+str(fontsize))
     print("max_length:"+str(max_length))
     print("v:"+str(v))
     print("acc:"+str(acc))
     print("colour="+colour)
+
+
+def fullscreen(fs):
+    def x(self):
+        global fs
+        if(fs):
+            root.wm_attributes("-fullscreen", False)
+            fs = False
+        else:
+            root.wm_attributes("-fullscreen", True)
+            fs = True
+    return x
 
 
 if __name__ == '__main__':
@@ -405,7 +428,9 @@ if __name__ == '__main__':
     enable_fav = False
     enable_rt = False
     realtime = False
+    rmwd = True
     colour = "white"
+    fs = False
 
     lefted = []
     config = configparser.ConfigParser()
@@ -447,6 +472,7 @@ if __name__ == '__main__':
     f = ttk.Frame(master=root, style="TP.TFrame", width=ww, height=wh)
     root.bind("<Control-Key-s>", Change_word(f))
     root.bind("<Control-Key-t>", tweetframe(word))
+    root.bind("<Control-Key-f>", fullscreen(fs))
     f.pack()
 
     j = 0
@@ -460,12 +486,16 @@ if __name__ == '__main__':
     for i in range(num_comment):
         if(i < len(nowdatalist)):
             if exURL(nowdatalist[i]) and (not longtxt(nowdatalist[i])):
-                nowdatalist[i].text.replace("\n", "")
+                if(rmwd):
+                    nowdatalist[i].text = nowdatalist[i].text.replace(
+                        "\n", "").replace(word.replace(" -filter:retweets", ""), "")
+                else:
+                    nowdatalist[i].text = nowdatalist[i].text.replace("\n", "")
             else:
                 nowdatalist[i].text = ""
                 lefted[i] = True
-            labels.append(ttk.Label(master=root, text=nowdatalist[i].text.replace(
-                "\n", " "), font=("メイリオ", fontsize), foreground=colour, background="grey"))
+            labels.append(ttk.Label(master=root, text=nowdatalist[i].text, font=(
+                "メイリオ", fontsize), foreground=colour, background="grey"))
         else:
             nowdatalist.append(nowdatalist[0])  # padding(改良の余地あり)
             nowdatalist[-1].text = ""  # padding
